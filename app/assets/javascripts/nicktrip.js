e0b99251adac0901daaf96067f8a5aed8c14d421
Blanchet <h55nick@gmail.com>
2013-03-06 16:13:57 -0500

    still building tripdiv

--- app/assets/javascripts/nicktrip.js
+++ app/assets/javascripts/nicktrip.js
@@ -10,7 +10,14 @@ function create_trip(trip_id){
   alert("You are creating a trip");
   trip = get_trip(trip_id);
   console.log("Creating- "+ trip);
+
   tripblock = $('</div>').addClass("trip");
+
+  name_div = $('<div>').addClass('trip_name').text(trip.name);
+  startdate_div = $('<div>').addClass('trip_startdate').text(trip.startdate);
+  enddate_div = $('<div>').addClass('trip_editdate').text(trip.editdate);
+
+  tripblock.append();
 
 
   $('.trips').append(tripblock);
