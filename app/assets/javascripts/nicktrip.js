0bf2a3cfabd8407aa865f220dd4791fbb5f0ea9d
Blanchet <h55nick@gmail.com>
2013-03-06 16:44:50 -0500

    basic show_trip

--- app/assets/javascripts/nicktrip.js
+++ app/assets/javascripts/nicktrip.js
@@ -11,15 +11,20 @@ function create_trip(trip_id){
   trip = get_trip(trip_id);
   console.log("Creating- "+ trip);
 
-  tripblock = $('</div>').addClass("trip");
+  tripblock = $('<div>').addClass("trip");
+  /*--  SHOW TRIP */
+  showtrip =$('<div>').addClass("showtrip");
+      name_div = $('<div>').addClass('trip_name').text(trip.name);
+      startdate_div = $('<div>').addClass('trip_startdate').text(trip.startdate);
+      enddate_div = $('<div>').addClass('trip_editdate').text(trip.editdate);
+  showtrip.append(name_div,startdate_div,enddate_div);
 
-  name_div = $('<div>').addClass('trip_name').text(trip.name);
-  startdate_div = $('<div>').addClass('trip_startdate').text(trip.startdate);
-  enddate_div = $('<div>').addClass('trip_editdate').text(trip.editdate);
+  /*---  Adding Buttons  ----*/
   buttonbox = $('<div>').addClass('buttons');
-    editbutton = $('<div>').addClass('.edit_trip_btn');
-    deletebutton = $('<div>').addClass('.delete_trip_btn');
+      editbutton = $('<div>').addClass('.edit_trip_btn');
+      deletebutton = $('<div>').addClass('.delete_trip_btn');
   buttonbox.append(editbutton,deletebutton);
+
 
   tripblock.append();
 
