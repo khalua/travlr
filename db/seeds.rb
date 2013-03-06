8f688d0230b86d6b0e3838c8c39cb72c9c2f6dee
Jane Sternbach <janesternbach@gmail.com>
2013-03-06 16:15:46 -0500

    updated models and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 User.delete_all
 Trip.delete_all
+Activity.delete_all
 
 u1 = User.create(:name => 'Tony', :email => 'tony.contreras@gmail.com', :password => 'a', :password_confirmation => 'a')
 u2 = User.create(:name => "Jane", :email => "jane@jane.com", :password => "abc", :password_confirmation => "abc")
