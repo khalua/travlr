9c72181069948fb77cf59a61f4a2b0272e180127
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-06 16:00:36 -0500

    Creating get_trip javascript function

--- app/assets/javascripts/st-trip.js
+++ app/assets/javascripts/st-trip.js
@@ -1,6 +1,21 @@
-function get_trip(trip_id)
+function get_trip(trip)
+{
+  var id = trip.id;
+
+  $.ajax({
+      dataType: 'json',
+      type: "get",
+      url: "/trips/" + id,
+    }).done();
+}
+
+function create_edit_trip(trip)
 {
 
 }
 
 function edit_trip()
+{
+}
+
+function delete_trip()
