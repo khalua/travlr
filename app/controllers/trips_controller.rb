b3032fc6988306db668c5294fc953deaae037251
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-06 17:19:14 -0500

    Changed trip_controller.rb

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,11 +1,7 @@
 class TripsController < ApplicationController
 
   def index
-    # @trips =  @auth.trips
-    t1 = Trip.create(:name => 'Trip 1', :startdate => '2012-03-01', :enddate => '2012-03-10')
-    t2 = Trip.create(:name => 'Trip 1', :startdate => '2012-03-11', :enddate => '2012-03-20')
-    t3 = Trip.create(:name => 'Trip 1', :startdate => '2012-03-21', :enddate => '2012-03-30')
-    @trips = [t1, t2, t3]
+    @trips =  @auth.trips
   end
 
   def show
