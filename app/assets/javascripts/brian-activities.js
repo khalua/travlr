b1a7cb289efe7b036e569ff7d9d0dad7ab251be9
Brian Yingling <brian_yingling@yahoo.com>
2013-03-06 19:50:27 -0500

    activities

--- app/assets/javascripts/brian-activities.js
+++ app/assets/javascripts/brian-activities.js
@@ -5,41 +5,77 @@ var activities = [];
 function create_activity() {
   var name = $('#activity-name').val();
   var description = $('#activity-description').val();
-  var
+  var image = $('#activity-image').val();
+  var trip_id = $('#trip_id').val();
+  $.ajax({
+    dataType: 'json',
+    type: 'post',
+    url: '/activities',
+    data:{authenticity_token:token,}
+
+  }).done({});
 }
 
 
 
 
 function process_activity() {
-
+  _.each(activities, display_activity)
 }
 
 
 function display_activity(activity) {
+  var $div_activity = $('<div>');
+  $div_activity.addClass('activites');
 
-  // displays the
-  $div_activity = $('div');
-  $div_activity.addClass('.activites');
-
-  $show_activity = $('div');
-  $show_activity.addClass('.show_activities');
+  var $show_activity = $('<div>');
+  $show_activity.addClass('show_activities');
 
   // populate divs with data from activity
-  $name = $('div');
+  var $updown = $('<div>');
+  $updown.addClass('updown')
+  var $up = $('<img>');
+  $up.attr('src','/assets/up.png').addClass('up');
+  var $down = $('<img>');
+  $down.attr('src','/assets/down.png').addClass('down');
+  $updown.append($up).append($down);
+
+  var $name = $('<div>');
   $name.html(activity.name);
-  $description= $('div');
+  //$name.text('activity.name');
+
+  var $description= $('<div>');
   $description.html(activity.description);
-  $category = $('div');
+  //$description.html('description');
+
+  var $category = $('<div>');
   $category.html(activity.category);
-  $image = $('img');
+
+  var $image = $('<img>');
   $image.attr('src',activity.image);
 
+  var $address = $('<div>');
+  $address.html(activity.address);
+
+  var $edit = $('<a>');
+  $edit.html('Edit');
+  $edit.addClass('button radius edit-activity-btn');
+
+  var $del= $('<a>');
+  $del.html('Delete');
+  $del.addClass('button radius delete-activity-btn');
+
+  var $activity_id = $('<input>');
+  $activity_id.attr('type','hidden');
+  $activity_id.html(activity.id);
+
   // append show_activity to activity div
-  $show_activity.append($name).append($description).append($category).append($image);
+  $show_activity.append($updown);
+  $show_activity.append($name);
+  $show_activity.append($description).append($category).append($image).append($edit).append($del).append($activity_id);
   $div_activity.append($show_activity);
 
   // display form IF user wishes to edit
-  $form_activity = %('div');
+  $form_activity = $('<div>');
 
 }
--- app/assets/javascripts/travlr.js
+++ app/assets/javascripts/travlr.js
@@ -2,9 +2,10 @@ $(function() {
   // event handlers
 
   // shows/hide activity form
-  $('.trip').click(process_activities);
+  $('.trip').click(process_activity);
 
   $('#activity_form').click();
+  process_activity();
 
 
 });
