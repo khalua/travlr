c65b37c5bcf0dad8c9d8dff74ee8655db8620eea
Brian Yingling <brian_yingling@yahoo.com>
2013-03-06 22:12:58 -0500

    modified new activity login

--- app/assets/javascripts/brian-activities.js
+++ app/assets/javascripts/brian-activities.js
@@ -5,6 +5,7 @@ function create_activity() {
   var name = $('#activity-name').val();
   var description = $('#activity-description').val();
   var image = $('#activity-image').val();
+  var address = $('#activity-address').val();
   var trip_id = $('#trip_id').val();
   $.ajax({
     dataType: 'json',
@@ -12,7 +13,7 @@ function create_activity() {
     url: '/activities',
     data:{authenticity_token:token}
 
-  }).done({});
+  }).done(process_activity);
 
 }
 
@@ -20,6 +21,7 @@ function create_activity() {
 
 
 function process_activity() {
+
   _.each(activities, display_activity)
 }
 
@@ -28,6 +30,8 @@ function process_activity() {
 function display_activity(activity) {
   var $div_activity = $('<div>');
   $div_activity.addClass('activites');
+
+  console.log($div_activity);
 
   var $show_activity = $('<div>');
   $show_activity.addClass('show_activities');
@@ -42,12 +46,12 @@ function display_activity(activity) {
   $updown.append($up).append($down);
 
   var $name = $('<div>');
-  $name.html(activity.name);
-  //$name.text('activity.name');
+  //$name.html(activity.name);
+  $name.text(activity.name);
 
   var $description= $('<div>');
+  //$description.html(activity.description);
   $description.html(activity.description);
-  //$description.html('description');
 
   var $category = $('<div>');
   $category.html(activity.category);
@@ -75,8 +79,32 @@ function display_activity(activity) {
   $show_activity.append($name);
   $show_activity.append($description).append($category).append($image).append($edit).append($del).append($activity_id);
   $div_activity.append($show_activity);
+  $(".tripactivities").append($div_activity);
 
   // display form IF user wishes to edit
   $form_activity = $('<div>');
-
 }
+
+// empties and displays the activity form before rendering it.
+// called when clicking the new activity form
+function show_activity_form() {
+  alert('got here');
+   $('#activity-name').val('');
+   $('#activity-description').val('');
+   $('#activity-image').val('');
+   $('#activity-address').val('');
+
+   // show activity form
+   $('.activity_form').show();
+
+   // append form after the new priority button
+   $('#new_activity').after($('.activity_form') );
+   return false;
+}
+
+
+
+
+
+
+
--- app/assets/javascripts/travlr.js
+++ app/assets/javascripts/travlr.js
@@ -3,12 +3,13 @@ $(function() {
 
   // shows/hide activity form
   $('.trip').click(process_activity);
+  $('#create_activity').click(create_activity);
 
 
-  $('#activity_form').click();
+
+  $('#new_activity').click(show_activity_form);
   process_activity();
 
 
 });
 
-var activities = [];
